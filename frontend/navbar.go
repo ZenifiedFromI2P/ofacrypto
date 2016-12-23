37be80fdd1c06a1af7acca65fade12b0ef285177
Zenified <zenified@riseup.net>
2016-12-23 02:38:53 +0000

    Add navbar initial template support, need to write template and use go-bindata on it

--- frontend/navbar.go
+++ frontend/navbar.go
@@ -1,5 +1,23 @@
 package frontend
 
 import (
+  "bytes"
+  "io/ioutil"
   "html/template"
+
+  "github.com/ZenifiedFromI2P/bindata"
 )
+
+const tmpl = html.New("navbar.html").Parse(string(bindata.Asset("navbar.html")))
+
+type TmplInputs struct {
+  IsDealer bool
+  Logged bool
+}
+
+func Generate(I TmplInputs) string {
+  out := new(bytes.Buffer)
+  tmpl.Execute(out, I)
+  b, _ := ioutil.ReadAll(out)
+  return string(b)
+}
